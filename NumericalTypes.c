0daeac4751080fd3d3d45658985e319de66a4a21
t-anano <ananor@aggies.ncat.edu>
2023-09-27 18:04:17 -0400

    Numerical types

---
+++ NumericalTypes.c
@@ -0,0 +1,19 @@
+#include <stdlib.h> 
+#include <stdio.h> 
+
+
+int main(){
+
+// Used to represent negative numbers, postiive numbers, and 0 
+//Signed integer types: signed char, short int, int, long int, and long long int
+
+// There is a corresponding unsigned type that uses the same amount of storage - They can only represent potive numbers and 0 
+// unsigned char, unsigned short int, unsigned int ,unsigned long int, unsigned long long int
+
+
+//Actual size limits specified in the limits <limits.h> header 
+
+// Enum Numerical Type: enum day{sun, mon, tue, wed, thurs, fri, sat};
+
+    return 0; 
+}
