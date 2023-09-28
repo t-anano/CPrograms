dcb97543528879675a299358495a3c027313a9ea
t-anano <ananor@aggies.ncat.edu>
2023-09-28 01:21:48 -0400

    Derived Types

---
+++ derived_types.c
@@ -0,0 +1,24 @@
+#include <stdlib.h> 
+#include <stdio.h> 
+
+
+//Derived types are constructed from other types - 
+// These include pointers, arrays, type definitions, structures, and unions
+
+// A pointer type is derived from the funciton or object it points to 
+// A pointer provides a reference to an entitiy of the referneced type 
+int *ip; 
+char *cp; 
+void *vp; 
+
+int i = 17; 
+int *ip = &i; 
+//* indirection operator - converts a type into a value of that type 
+// & adress of operator 
+ip = &*ip; // ip is the adress of the value at ip which is the adress of i 
+
+
+//An array is a contiguously allocated sequence of objects with the same element type
+
+int ia[11]; // Array of 11 ints 
+float *afp[17]; // 17 elements of type pointer to float 
