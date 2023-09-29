4018d494069f26b75805cd2b27afc527d2107bb2
t-anano <ananor@aggies.ncat.edu>
2023-09-29 17:35:59 -0400

    Arrays/multidimensional arrays as pointers and indirections

--- derived_types.c
+++ derived_types.c
@@ -23,11 +23,27 @@ ip = &*ip; // ip is the adress of the value at ip which is the adress of i
 int ia[11]; // Array of 11 ints 
 float *afp[17]; // 17 elements of type pointer to float
 
+
+void func(int arr[5]); 
 int main(){
 
     char str[11];
     for(unsigned int i = 0; i < 10;i++){
-       str[i] = '0' + i; // Yields a number 
+       str[i] = '0' + i; // definition is such str[i] == *(str + i) = '0' + i 
+       // Using brackets with arrays is like derefrenceing an offset from the start of the array
+
+
+       //A Multidimesnional Array is called a Matrix 
+       unsigned int i = 0; 
+       unsigned int j = 0; 
+
+       int arr[3][5]; //Arr is a list of three elements each of which is an array of 5 elements 
+       func(arr[i]); // 1. arr is converted into a pointer to the initial array of 5 elements 2. i is scaled to represent 5 3. Steps 1 and 2 are added 4.  indirection is applied to the result 
+       int x = arr[i][j]; // arr is converted into a pointer to the first element of type int 
+
+       
+
+       return 0;
     }
 
     str[10] = '\n'; 
