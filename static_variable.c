4f5539ccb9e6c8748116fa35a7150b48940fef17
t-anano <ananor@aggies.ncat.edu>
2023-09-30 19:57:16 -0400

    Chapter 2 Exercises

--- static_variable.c
+++ static_variable.c
@@ -1,13 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
  
- // a static objects inside a function keeps its value between invocations 
+  static unsigned int counter = 0;
+ //static objects inside a function keeps its value between invocations 
  void increment(void){
-   static unsigned int counter = 0; 
    counter++; 
-   printf("%d\n", counter); 
-
+   printf("%d\n", counter);
  }
+
+ int retrieve(){
+   return counter; 
+ }
+
+
 
  // static objects must be initialized to a constant and not a variable 
  int* func(int i ){
@@ -16,9 +21,33 @@
 
  }
 
+
+// Exercise 1: Write an array of 3 function pointers that execute based on the passed index 
+int sum(int a, int b){return a + b;}
+int subtract(int a, int b){return a - b;}
+int mul(int a, int b){return a*b;}
+ int (*p[3])(int x, int y); // array of three pointers to int functions 
+
  int main(void){
-    for(int i = 0; i < 5;i++){
+    for(int i = 0; i < 5;i++){ 
+       printf("%d\n", retrieve());
        increment(); // as you can see, incrment is called 5 times and since counter is static it retains its value amongst invokations and goes from 0 to 5 as the funciton is called 5 times 
+       
     }
+
+   p[0] = sum; 
+   p[1] = subtract; 
+   p[2] = mul; 
+
+   int op; 
+   int a = 20; 
+   int b = 5; 
+   int result; 
+
+   for (op = 0; op < 3; op++) {
+         result = p[op](a, b);
+         printf("Result: %d\n", result);
+      }
+      
     return 0; 
  }
